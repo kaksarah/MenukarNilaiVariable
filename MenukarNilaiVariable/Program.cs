3fb8ec38acb5467022014b96c1a446fbb994bf86
NUR <ter.realbe.kan@gmail.com>
2022-12-19 14:35:50 +0700

    Add project files.

---
+++ MenukarNilaiVariable/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int a = 15;
+        int b = 20;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        
+
+        Console.Write("{0} - {1}", a, b);
+    }
+}
